83ab73aab356e5ea537f0e70f3abf86a96cf88c1
Bozorov06 <qoyilbekbozorov160@gmail.com>
2024-12-04 08:20:16 +0500

    ulandi

---
+++ main.js
@@ -0,0 +1,584 @@
+// ==================================================
+// 1-masala
+// Funksiya yozing, u berilgan sonning juft yoki toq ekanligini aniqlasin.
+// function sum(num) {
+//     if (num%2==0){  
+//         return "juft son"; 
+//     }else{
+//         return "toq son";
+//     }
+    
+// }
+//     console.log(sum(12));
+
+// ==================================================
+// 2-masala
+// Funksiya yozing, u berilgan sonning musbat, manfiy yoki nol ekanligini aniqlasin.
+// function sum(n) {
+//     if(n>0){
+//         return "musbat"
+//     }else{
+//         return "manfiy"
+//     }
+// }
+// console.log(sum());
+
+
+// ==================================================
+// 3-masala
+// Funksiya yozing, u berilgan ikki sondan kattasini qaytarsin.
+// function sum(n,m) {
+//     if (n>m) {
+//         return "n"
+//     }else{
+//         return "m"
+//     }
+// }
+// console.log(sum(2,3));
+
+// ==================================================
+// 4-masala
+// Funksiya yozing, u uchta sondan eng kattasini aniqlasin.
+// function sum(a,b,c) {
+    
+// }
+
+// ==================================================
+// 5-masala
+// Funksiya yozing, u berilgan sonning 3 ga bo‘linishini aniqlasin.
+// function sum(n) {
+//     if (n%3==0) {
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// console.log(sum());
+
+
+// ==================================================
+// 6-masala
+// Funksiya yozing, u berilgan sonning kvadratini qaytarsin.
+// function sum(n) {
+//     res=n**2
+//         return res;
+// }
+
+// console.log(sum());
+
+
+// ==================================================
+// 7-masala
+// Funksiya yozing, u ikki sonning ko‘paytmasini hisoblasin.
+// function sum(a,b) {
+//     res = a*b
+
+//     return res
+// }
+
+// console.log(sum());
+
+
+// ==================================================
+// 8-masala
+// Funksiya yozing, u berilgan sonni 1 dan boshlab o‘sha songacha bo‘lgan barcha sonlarning yig‘indisini hisoblasin.
+// let sum = 0;
+// function sumfull(n) {
+//     for (let i =1; i<=n; i++){
+//         sum+=i;
+
+//     }
+//     return sum
+// }
+
+// console.log(sumfull());
+
+
+// ==================================================
+// 9-masala
+// Funksiya yozing, u 1 dan 10 gacha bo‘lgan sonlarning ko‘paytmasini qaytarsin.
+// let k = 1;
+// function sumfull(num1=1,num2=10) {
+//     for ( let i = num1; i<=num2; i++){
+//         k*=i
+//     }
+//     return k
+// }
+// console.log(sumfull());
+
+
+// ==================================================
+// 10-masala
+// Funksiya yozing, u berilgan sondan boshlab 1 ga kamaytirib chiqaradi.
+// function sum(n) {
+//     while (n >= 1) {
+//         console.log(n);
+//         n--
+//     }
+// }
+// sum()
+
+
+
+
+// ==================================================
+// 11-masala
+// Funksiya yozing, u berilgan sonni faqat juft bo‘lsa ekranga chiqaradi.
+// function sum(num) {
+//     if (num%2==0) {
+//         return num
+//     } else {
+//         return false
+//     }
+// }
+// console.log(sum(3));
+
+
+// ==================================================
+// 12-masala
+// Funksiya yozing, u berilgan ikki sondan kattasini ternary operator yordamida aniqlasin.
+// function sum(n, m) {
+//     n>m ? console.log(true) : console.log(false);
+    
+// }
+// sum()
+
+// ==================================================
+// 13-masala
+// Funksiya yozing, u berilgan son 5 ga va 3 ga bo‘linishini tekshiradi.
+// function sum(n) {
+//     if(n%5==0 && n%3==0){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(sum());
+
+
+// ==================================================
+// 14-masala
+// Funksiya yozing, u berilgan sonning kvadrati va kubini qaytaradi.
+// function sum(num) {
+//     return num**2 && num**3;
+// }
+// console.log(sum());
+// shuni kiritganimda kubini uzi chiqib qolyapti
+
+
+// ==================================================
+// 15-masala
+// Funksiya yozing, u 1 dan 100 gacha bo‘lgan faqat juft sonlarni ekranga chiqaradi.
+// function sum() {
+//     for (let i = 2; i<=100; i+=2){
+//         console.log(i);
+        
+//     }
+// }
+// sum()
+
+
+
+// ==================================================
+// 16-masala
+// Funksiya yozing, u 1 dan n gacha bo‘lgan toq sonlarni ekranga chiqaradi
+// function sum(n) {
+//     for ( let i =1; i<=n; i+=2){
+//         console.log(i);
+//     }
+// }
+
+// sum()
+
+// ==================================================
+// 17-masala
+// Funksiya yozing, u kiritilgan sonning faktorialini hisoblaydi.
+// let f= 1
+// function sum(n) {
+//     for (let i = 1; i <=n; i++) {
+//         f *= i
+//     }
+//     return f
+// }
+// console.log(sum());
+
+
+// ==================================================
+// 18-masala
+// Funksiya yozing, u kiritilgan sonni 10 ga bo‘lgandagi qoldiqni qaytaradi
+// function sum(num) {
+//     return num%10;
+// }
+// console.log(sum());
+
+
+
+// ==================================================
+// 19-masala
+// Funksiya yozing, u ikki sonni o‘zaro solishtirib, qaysi biri katta ekanligini ekranga chiqaradi.
+// function sum(num1,num2) {
+//     if(num1>num2){
+//         return num1
+//     }else{
+//         return num2
+//     }
+// }
+// console.log(sum());
+
+// ==================================================
+// 20-masala
+// Funksiya yozing, u 1 dan 50 gacha bo‘lgan barcha sonlarning yig‘indisini qaytaradi.
+// let sum =0;
+// function sumfull() {
+//     for (let i =1; i<=50; i++){
+//         sum += i
+//     }
+//     return sum;
+// }
+// console.log(sumfull());
+
+// ==================================================
+// 21-masala
+// Funksiya yozing, u berilgan sonning kvadrat ildizini hisoblaydi (math metodlari ishlatilmasin).
+// function sum(n) {
+//     return Math.sqrt(n)
+// }
+// console.log(sum());
+
+
+
+// ==================================================
+// 22-masala
+// Funksiya yozing, u ikkita sonning o‘rta arifmetik qiymatini hisoblaydi.
+// function sumfull(n,m) {
+//     let sum = n + m
+//     return sum / 2
+// }
+// console.log(sumfull());
+
+
+// ==================================================
+// 23-masala
+// Funksiya yozing, u berilgan son musbatmi yoki manfiy ekanligini if orqali aniqlasin.
+// function sum(n) {
+//     if (n>0) {
+//         return 'musbat'
+//     }else{
+//         return 'manfiy'
+//     }
+// }
+// console.log(sum(5));
+
+
+// ==================================================
+// 24-masala
+// Funksiya yozing, u berilgan sonni juft sonlar yig‘indisini hisoblaydi.
+// savolga tushunmadim
+
+// ==================================================
+// 25-masala
+// Funksiya yozing, u ikki sonni qo‘shadi, lekin agar sonlardan biri manfiy bo‘lsa, natijaga 10 qo‘shadi.
+// function sum(a,b) {
+//     if(a+b>0 || !(a<0 || b<0)){
+//         return true
+//     }else{
+//         return +10
+//     }
+// }
+// console.log(sum());
+
+
+// ==================================================
+// 26-masala
+// Funksiya yozing, u berilgan ikkita sonni qo‘shadi va ko‘paytiradi, natijalarni alohida qaytaradi.
+// let sum = 0;
+// let k = 1;
+// function sumfull(n,m) {
+//     sum = n+m
+//     k = n*m
+//     console.log(sum);
+//     console.log(k);
+// }
+// sumfull(2,4)
+
+// ==================================================
+// 27-masala
+// Funksiya yozing, u foydalanuvchi kiritgan son 7 ga bo‘linishini aniqlaydi.
+// function sum(n) {
+//     if (n%7==0){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(sum());
+
+
+// ==================================================
+// 28-masala
+// Funksiya yozing, u berilgan sonni xuddi shu tartibda 10 marta ko‘paytirib chiqaradi.
+// savolga tushunmadim
+
+// ==================================================
+// 29-masala
+// Funksiya yozing, u ikki sonni ko‘paytiradi, lekin agar sonlardan biri 0 bo‘lsa, natija 1 bo‘ladi.
+// function sum(n,m) {
+//     if(n == 0 || m == 0){
+//         return 1;
+//     }
+//      return n * m;
+// }
+// console.log(sum(2,3));
+
+
+// ==================================================
+// 30-masala
+// Funksiya yozing, u foydalanuvchi kiritgan ikkita sonning modulini hisoblaydi.
+// qanday qilishni bilmadim
+
+
+// ==================================================
+// 31-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonni faqat manfiy bo‘lsa ekranga chiqaradi.
+// let num = +prompt("sonni kiriting")
+// function sum() {
+//     if (num<0){
+//         return "manfiy"
+//     }else{
+//         return "manfiy emas"
+//     }
+// }
+// console.log(sum());
+
+
+// ==================================================
+// 32-masala
+// Funksiya yozing, u 1 dan 100 gacha bo‘lgan sonlardan faqat 5 ga karrali bo‘lganlarni ekranga chiqaradi.
+// function sum() {
+//     for (let i = 1; i <= 100; i++) {
+//       return i%5==0
+//     }
+// }
+// console.log(sum());
+
+
+// ==================================================
+// 33-masala
+// Funksiya yozing, u berilgan ikki sonning yig‘indisini hisoblaydi, agar natija juft bo‘lsa, "Juft" deb qaytaradi
+// let sum = 0;
+// let res;
+// function sumfull(n,m) { 
+//     sum = n+m
+//     res = sum%2==0
+//     return res;
+// }
+
+// console.log(sumfull(5,3));
+
+
+// ==================================================
+// 34-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonning bo‘luvchilarini ekranga chiqaradi.
+// let num  = +prompt("sonni kiriting")
+// function sum() {
+    
+// }
+
+// ==================================================
+// 35-masala
+// Funksiya yozing, u berilgan sondan kattalik bo‘yicha kichik bo‘lgan barcha toq sonlarni chiqaradi.
+// function sum() {
+    
+// }
+
+// ==================================================
+// 36-masala
+// Funksiya yozing, u berilgan sonni 1 dan 20 gacha bo‘lgan sonlarga bo‘lib, har birining qoldig‘ini chiqaradi.
+// let res;
+// function sum(n) {
+//     for(let i = 1; i<= 20; i++){
+//         res = n % i
+//     }
+//     return res;
+// }
+// console.log(sum(12));
+
+
+// ==================================================
+// 37-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonning barcha bo‘luvchilarini hisoblaydi va yig‘indisini qaytaradi.
+// function sumfull(num) {
+//     let sum = 0
+//     for (let i = 1; i <=num; i++) {
+//         if ( num%i == 0) {
+//             sum += i
+//         }
+//     }
+//     console.log(sum);
+// }
+// sumfull()
+
+
+// ==================================================
+// 38-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonni faqat juft bo‘lsa kvadratini hisoblaydi.
+// let num = +prompt('sonni kiriting')
+// let k = 1; 
+// function sum() {
+//     if (num%2==0){
+//         k = num**2
+//         console.log(k);
+//         return k 
+//     }else{
+//         console.log("juft son emas");
+//     }
+// }
+// sum()
+
+// ==================================================
+// 39-masala
+// Funksiya yozing, u berilgan son 100 dan katta bo‘lsa, uni ikki barobar oshiradi, aks holda 10 qo‘shadi.
+// function sum(n) {
+//     if (n>100){
+//        console.log(n*2);
+//     }else{
+//         console.log(n+10);
+//     }
+// }
+// console.log(sum(12));
+
+
+// ==================================================
+// 40-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonning barcha raqamlarini birma-bir chiqaradi.
+// let num = +prompt('sonni kiriting')
+// function sum() {
+// }
+//tushunmadim
+
+
+// ==================================================
+// 41-masala
+// Funksiya yozing, u foydalanuvchi kiritgan ikki sonning o‘zaro karrali ekanligini aniqlaydi.
+// let num = +prompt("sonni kiriting");
+// let num2 = +prompt("sonni kiriting");
+// function sum() {
+//     if (num%num2==0) {
+//         console.log(true);
+//     }else{
+//         console.log(false); 
+//     }
+// }
+
+// sum()
+// ==================================================
+// 42-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonning raqamlari yig‘indisini hisoblaydi.
+// let num = +prompt("sonni kiriting");
+// function sumfull() {
+    
+// }
+
+
+// ==================================================
+// 43-masala
+// Funksiya yozing, u 1 dan foydalanuvchi kiritgan sondan katta bo‘lgan barcha sonlarni ekranga chiqaradi.
+// let num = +prompt("sonni kiriting");
+// function sum() {
+// }
+// savol notugriga uxshayapti
+
+// ==================================================
+// 44-masala
+// Funksiya yozing, u foydalanuvchi kiritgan ikkita sonning bo‘linmasini hisoblaydi (qoldiqsiz).
+// let num = +prompt("sonni kiriting");
+// let num2 = +prompt("sonni kiriting");
+// function sum() {
+    
+// }
+
+// ==================================================
+// 45-masala
+// Funksiya yozing, u 1 dan n gacha bo‘lgan barcha sonlarning kvadratlarini ekranga chiqaradi.
+// function sum(n) {
+//     while (n>0) {
+
+//     }
+//     console.log(res);
+    
+// }
+
+// sum(10)
+
+
+// ==================================================
+// 46-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonning o‘z-o‘ziga karraligini aniqlaydi.
+// let num = +prompt("sonni kiriting");
+// function sum() {
+//     if (num%num==0) {
+//         console.log(true);
+        
+//     }else{
+//         console.log(false);
+        
+//     }
+// }
+// sum()
+// ==================================================
+// 47-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonning har bir raqamini alohida kvadratga oshiradi va chiqaradi.
+// function sum(num) {
+//     while (num>=1) {
+//         let lastnum = son%10;
+//         console.log(lastnum**2);
+//         num = (Math.trunc(num/10))
+//     }
+// }
+// sum()
+
+// ==================================================
+// 48-masala
+// Funksiya yozing, u 1 dan 50 gacha bo‘lgan barcha sonlarning faqat toqlari yig‘indisini hisoblaydi.
+// let sum = 0; 
+// function sumfull() {
+//     for(let i =1; i<=50; i++){
+//         if (i%2==1){
+//             console.log(i);
+//             sum += i
+//         }
+    
+//     }
+//     console.log(sum);
+// }
+// sumfull()
+
+// ==================================================
+// 49-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonni teskari tartibda chiqaradi.
+// let num = +prompt("sonni kiriting")
+// function sum() {
+//     for (let i = num;  i>=1; i--){
+//         console.log(i);
+//     }
+// }
+// sum()
+
+// ==================================================
+// 50-masala
+// Funksiya yozing, u foydalanuvchi kiritgan sonning raqamlari yig‘indisi 10 dan katta yoki kichikligini aniqlaydi.
+// function sumfull(num) {
+//     let sum = 0;
+//     while ( num>=1 ) {
+//         let lastnum = num%10
+//         num = (Math.trunc(num/10))
+//         sum += lastnum
+//         if(sum < 10){
+//             return "yig'indi 10dan katta"
+//         }else{
+//             return "yig'indi 10dan kichik"
+//         }
+//     }
+// }
+// console.log(sum());
